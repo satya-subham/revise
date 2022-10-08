d74244fb79ff45be3bcae1f2648646165e39fea1
satya-subham <satyasubham85@gmail.com>
2022-10-08 20:29:37 +0530

    revise

---
+++ ARRAY/firstnonreptnum.js
@@ -0,0 +1,23 @@
+// repeating numbers
+// let left = [1,3,4,3,2,5,7,1]
+// let right = []
+// for (let i = 0; i < left.length; i++) {
+//     if(right.includes(left[i])){
+//         console.log(left[i]);
+//     }else{
+//         right.push(left[i])
+//     }
+// }
+
+
+// first repeating numbers (if you don't add break it will print all repeating number)
+let left = [1,3,4,3,2,5,7,1]
+let right = []
+for (let i = 0; i < left.length; i++) {
+    if(right.includes(left[i])){
+        console.log(left[i]);
+        break;
+    }else{
+        right.push(left[i])
+    }
+}
