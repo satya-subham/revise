1e140bc0657113ee33262e2e97b5319079cd6e98
satya-subham <satyasubham85@gmail.com>
2022-10-09 13:11:00 +0530

    revise

--- ARRAY/firstnonreptnum.js
+++ ARRAY/firstnonreptnum.js
@@ -13,14 +13,47 @@
 // ==============================================================
 
 // first repeating numbers (if you don't add break it will print all repeating number)
-let left = [1,3,4,3,2,5,7,1]
-for (let i = 0; i < left.length; i++) {
-    if(left.includes(left[i], i+1)){
-        console.log(left[i]);
-        break;
-    }
-}
+// let left = [1,3,4,3,2,5,7,1]
+// for (let i = 0; i < left.length; i++) {
+//     if(left.includes(left[i], i+1)){
+//         console.log(left[i]);
+//         break;
+//     }
+// }
 
 
 
 // =================================================================
+
+// first non repeating numbers (if you don't add break it will print all non repeating number)
+// let left = [1,3,4,3,2,5,7,1]
+// for (let i = 0; i < left.length; i++) {
+//     if(!left.includes(left[i], i+1)){
+//         console.log(left[i]);
+//         break;
+//     }
+// }
+
+
+
+// =================================================================
+
+// Largest number
+// let nums = [1, 20, 32, 17, 89, 10, 27, 100];
+// let max = nums[0];
+// for(let i=1; i<nums.length; i++){
+//     if(max < nums[i]){
+//         max = nums[i]
+//     }
+// }
+// console.log(max);
+
+// Largest number (negative value)
+let nums = [-1, -20, -32, -17, -89, -10, -27, -100];
+let max = nums[0];
+for(let i=1; i<nums.length; i++){
+    if(max < nums[i]){
+        max = nums[i]
+    }
+}
+console.log(max);
